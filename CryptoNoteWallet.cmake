509951ad9bb5a9c571f4024cad2032c4a951c782
BitcoinSolution <bitcoinsolution@gmail.com>
2017-12-09 22:12:35 +0100

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "gaulois")
+set(CN_CURRENCY_DISPLAY_NAME "LeGaulois")
+set(CN_CURRENCY_TICKER "LeGaulois")
